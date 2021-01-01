adc029623ee76e31fb82358b86180ecd9641f7b5
vino <naokimidori@163.com>
2021-01-01 17:12:51 +0800

    feat: init

---
+++ src/pages/Index.vue
@@ -0,0 +1,42 @@
+<template>
+  <Layout :show-logo="true">
+    <!-- Author intro -->
+    <Author :show-title="true" />
+
+  </Layout>
+</template>
+
+<page-query>
+query {
+  posts: allPost(filter: { published: { eq: true }}) {
+    edges {
+      node {
+        id
+        title
+        date (format: "D. MMMM YYYY")
+        timeToRead
+        description
+        cover_image (width: 770, height: 380, blur: 10)
+        path
+        tags {
+          id
+          title
+          path
+        }
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+import Author from '~/components/Author.vue'
+import PostCard from '~/components/PostCard.vue'
+
+export default {
+  components: {
+    Author,
+    PostCard
+  }
+}
+</script>
