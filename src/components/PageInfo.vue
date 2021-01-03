f291cd7cbdd6fbc1d20a491da772319183d0c5d8
vino <naokimidori@163.com>
2021-01-03 17:32:15 +0800

    feat: 封装pager组件

---
+++ src/components/PageInfo.vue
@@ -0,0 +1,27 @@
+<template>
+  <Pager
+    :info="pageInfo"
+    linkClass="pager_link"
+  />
+</template>
+
+<script>
+import { Pager } from 'gridsome'
+export default {
+  props: ['pageInfo'],
+  components: {
+    Pager,
+  }
+}
+</script>
+
+<style lang="scss">
+.pager_link {
+  color: var(--body-color);
+  margin: 0 4px;
+  text-decoration: none;
+}
+.active--exact {
+  color: #8ca8be !important;
+}
+</style>
