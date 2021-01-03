9d0859390357b3eaca113a9138ecc06191282dcb
vino <naokimidori@163.com>
2021-01-03 17:32:49 +0800

    feat: posts page

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,17 +1,11 @@
 <template>
-  <div class="post-card content-box" :class="{'post-card--has-poster' : post.poster}">
-    <div class="post-card__header">
-      <g-image alt="Cover image" v-if="post.cover_image" class="post-card__image" :src="post.cover_image" />
-    </div>
-    <div class="post-card__content">
-      <h2 class="post-card__title" v-html="post.title" />
-      <p class="post-card__description" v-html="post.description" />
-
-      <PostMeta class="post-card__meta" :post="post" />
-      <PostTags class="post-card__tags" :post="post" />
-
-      <g-link class="post-card__link" :to="post.path">Link</g-link>
-    </div>
+  <div class="card_wrapper">
+     <div class="card_title">
+        <g-link class="card_title_link" :to="post.path">
+          {{post.title}}
+        </g-link>
+     </div>
+    <div class="card_date">{{post.date}}</div>
   </div>
 </template>
 
@@ -29,51 +23,37 @@ export default {
 </script>
 
 <style lang="scss">
-.post-card {
-  margin-bottom: var(--space);
-  position: relative;
+.card_wrapper {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-bottom: calc(var(--space) / 3);
+  // font-family: Consolas, Menlo, Monaco,  "Courier New", monospace;
+  font-family: Menlo;
 
-  &__header {
-    margin-left: calc(var(--space) * -1);
-    margin-right: calc(var(--space) * -1);
-    margin-bottom: calc(var(--space) / 2);
-    margin-top: calc(var(--space) * -1);
+  .card_title {
+    font-size: 0.8em;
+    flex: 1;
+    text-overflow: ellipsis;
     overflow: hidden;
-    border-radius: var(--radius) var(--radius) 0 0;
+    white-space: nowrap;
+    padding-right: 2em;
 
-    &:empty {
-      display: none;
+    &_link {
+      text-decoration: none;
+      color: var(--body-color);
+
+      &:hover {
+        color: #49b1f5;
+      }
     }
   }
 
-  &__image {
-    min-width: 100%;
-  }
-
-  &__title {
-    margin-top: 0;
-  }
-
-  &:hover {
-    transform: translateY(-5px);
-    box-shadow: 1px 10px 30px 0 rgba(0,0,0,.1);
-  }
-
-  &__tags {
-    z-index: 1;
-    position: relative;
-  }
-
-  &__link {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    opacity: 0.0;
-    overflow: hidden;
-    text-indent: -9999px;
-    z-index: 0;
+  .card_date {
+    font-size: 0.65em;
+    color: var(--body-color);
+    opacity: .6;
   }
 }
+
 </style>
--- src/pages/Posts.vue
+++ src/pages/Posts.vue
@@ -1,20 +1,32 @@
 <template>
   <Layout>
-    <div class="posts">
-      <PostCard v-for="edge in $page.posts.edges" :key="edge.node.id" :post="edge.node"/>
+    <div class="posts_page_wrapper">
+      <div class="posts_list">
+        <PostCard
+          v-for="edge in $page.posts.edges"
+          :key="edge.node.id"
+          :post="edge.node"
+        />
+      </div>
+      <div class="posts_footer">
+        <PageInfo :pageInfo="$page.posts.pageInfo" />
+      </div>
     </div>
-
   </Layout>
 </template>
 
 <page-query>
-query {
-  posts: allPost(filter: { published: { eq: true }}) {
+query ($page: Int) {
+  posts: allPost(perPage: 15, page: $page) @paginate {
+    pageInfo {
+      currentPage
+      totalPages
+    }
     edges {
       node {
         id
         title
-        date (format: "D. MMMM YYYY")
+        date (format: "YYYY/MM/DD")
         timeToRead
         description
         cover_image (width: 770, height: 380, blur: 10)
@@ -33,14 +45,38 @@ query {
 <script>
 import Author from '~/components/Author.vue'
 import PostCard from '~/components/PostCard.vue'
+import PageInfo from '~/components/PageInfo.vue'
 
 export default {
   components: {
     Author,
-    PostCard
+    PostCard,
+    PageInfo,
   },
   metaInfo: {
-    title: 'Hello, world!'
+    title: 'Posts'
   }
 }
 </script>
+<style lang="scss">
+.posts_page_wrapper {
+  max-width: 800px;
+  margin: 0 auto;
+  .posts_list {
+    margin: 0 auto;
+  }
+  
+  .posts_footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+	  padding: calc(var(--space) / 2) 0;
+
+    .pager_link {
+      color: var(--body-color);
+      margin: 0 4px;
+      text-decoration: none;
+    }
+  }
+}
+</style>
