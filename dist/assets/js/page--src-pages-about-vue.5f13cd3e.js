041f133a7c20725df2983b1199deebd8d3323597
vino <naokimidori@163.com>
2021-01-12 22:00:26 +0800

    add: dist

---
+++ dist/assets/js/page--src-pages-about-vue.5f13cd3e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{odGf:function(t,e,n){"use strict";n.r(e);var s={},u=n("XSKF"),i=Object(u.a)(s,(function(){var t=this.$createElement;return(this._self._c||t)("div",[this._v("PageAbout")])}),[],!1,null,null,null);e.default=i.exports}}]);
