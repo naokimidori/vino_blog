b00312c5e945e9446a9e84fbd2ddfde57686375a
vino <naokimidori@163.com>
2021-01-20 23:08:39 +0800

    feat: about页改造

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,13 +1,35 @@
 <template>
-  <div>PageAbout</div>
+  <Layout>
+    <div class="page_about_wrapper">
+      <div class="markdown-body" v-html="$page.post.content" />
+    </div>
+  </Layout>
 </template>
+
+<page-query>
+query Post {
+  post: post (id: "c242e969ba9cdad7dfa91f2c8b4a91e2") {
+    content
+  }
+}
+</page-query>
 
 <script>
 export default {
-
+  metaInfo: {
+    title: 'About Me'
+  }
 }
 </script>
+<style lang="scss">
+  .page_about_wrapper {
+    max-width: 800px;
+    margin: 0 auto;
+    box-sizing: border-box;
+    padding: 0 20px;
 
-<style>
-
+    h1 {
+      text-align: center;
+    }
+  }
 </style>
